dbcf20a99e819fd6bb6082895f8b916a36c68653
JUDY <s2018s05@e-mirim.hs.kr>
2020-04-28 14:21:54 +0900

    c#기본

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,22 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            var name = "서" + "예" + "진";
+            Console.WriteLine("Hello C#");
+            int add = 2;
+            Console.WriteLine("Hello World");
+            Console.Write("Hello World");
+            Console.WriteLine("Hello World");
+
+            Console.Write(52);
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("가"+"나"+"다");
+
+            Console.WriteLine("안녕"[100]);
+
+            Console.WriteLine();
+            Console.WriteLine("안녕"[100]);
         }
     }
 }
